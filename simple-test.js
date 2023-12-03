68ec90a973b20f8a86f789ea5e594ca59c107615
LongYinan <lynweklm@gmail.com>
2023-12-03 23:49:31 +0800

    test: add tar.xz unpack test

--- simple-test.js
+++
@@ -1,9 +0,0 @@
-const { Archive } = require('./index')
-
-const archive = new Archive(__dirname + '/__test__/src.tar')
-
-for (const entry of archive.entries()) {
-  console.info(entry.path())
-}
-
-console.info('Simple test passed')
