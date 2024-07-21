9e11ff313ec6b6c503c8b7f5337bb0cdd56b9388
LongYinan <lynweklm@gmail.com>
2024-07-21 15:37:48 +0800

    fix

--- benchmark/bench.ts
+++ benchmark/bench.ts
@@ -1,10 +1,13 @@
 import { readFileSync } from 'node:fs'
 import { join } from 'node:path'
 
-import b from 'benny'
+import { Bench } from 'tinybench'
 import { list } from 'tar'
 
 import { Archive } from '../index'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = join(fileURLToPath(import.meta.url), '..')
 
 const ARCHIVE_PATH = join(__dirname, '..', '__test__', 'src.tar.gz')
 
@@ -22,33 +25,26 @@ for (const entry of archive.entries()) {
   console.info('list from @napi-rs/tar', entry.path())
 }
 
-async function run() {
-  await b.suite(
-    'Read all entries',
+const b = new Bench()
 
-    b.add('@napi-rs/tar', () => {
-      const archiveBuffer = readFileSync(ARCHIVE_PATH)
-      const archive = new Archive(archiveBuffer)
-      for (const entry of archive.entries()) {
-        entry.path()
-      }
-    }),
-
-    b.add('node-tar', () => {
-      list({
-        file: join(__dirname, '..', '__test__', 'src.tar.gz'),
-        onentry: (entry) => {
-          entry.path
-        },
-        sync: true,
-      })
-    }),
-
-    b.cycle(),
-    b.complete(),
-  )
-}
-
-run().catch((e) => {
-  console.error(e)
+b.add('@napi-rs/tar', () => {
+  const archiveBuffer = readFileSync(ARCHIVE_PATH)
+  const archive = new Archive(archiveBuffer)
+  for (const entry of archive.entries()) {
+    entry.path()
+  }
 })
+
+b.add('node-tar', () => {
+  list({
+    file: join(__dirname, '..', '__test__', 'src.tar.gz'),
+    onentry: (entry) => {
+      entry.path
+    },
+    sync: true,
+  })
+})
+
+await b.run()
+
+console.table(b.table())
